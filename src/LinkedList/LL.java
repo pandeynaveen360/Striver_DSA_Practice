e718a273ef486d936e97d416494ae3878fc68797
naveenpandey <naveenppandey8080@gmail.com>
2025-11-03 21:38:15 +0530

    LinkedList

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -20,6 +20,24 @@ public class LL {
         size += 1;
     }
 
+    public void insert(int val, int index){
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        if(index == size){
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+        for(int i = 1; i < index; i++){
+            temp = temp.next;
+        }
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+        size++;
+    }
+
     public void insertLast(int val){
         if(tail == null){
             insertFirst(val);
