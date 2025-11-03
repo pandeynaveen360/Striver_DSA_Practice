0bd2e512e0e0bb4dd1808845afb1b6402c5ac03b
naveenpandey <naveenppandey8080@gmail.com>
2025-11-03 09:30:50 +0530

    LinkedList

---
+++ src/LinkedList/LL.java
@@ -0,0 +1,47 @@
+package LinkedList;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+    public LL(){
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == head) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void Display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+}
