d374d48d09e1f56fcd192a97e1ed671da4da5cef
naveenpandey <naveenppandey8080@gmail.com>
2025-11-05 10:25:06 +0530

    DoubllyLL

--- src/LinkedList/DoublyLL/DLL.java
+++ src/LinkedList/DoublyLL/DLL.java
@@ -16,11 +16,37 @@ public class DLL {
 
     public void display(){
         Node node = head;
+        Node last = null;
         while(node != null){
             System.out.print(node.val + " -> ");
+            last = node;
             node = node.next;
         }
         System.out.println("END");
+
+        System.out.println("Print in reverse");
+        while(last != null){
+            System.out.print(last.val + " -> ");
+            last = last.prev;
+        }
+        System.out.println("START");
+    }
+
+    public void insertLast(int val){
+        Node node = new Node(val);
+        Node last = head;
+
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return;
+        }
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
+
     }
 
     private class Node{
