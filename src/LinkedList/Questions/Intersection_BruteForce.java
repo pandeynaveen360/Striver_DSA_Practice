ee8eab063547e12ff99aa947a37d08e640044436
naveenpandey <naveenppandey8080@gmail.com>
2025-11-06 21:46:22 +0530

    Remove_Duplicates

--- src/LinkedList/Questions/Intersection_BruteForce.java
+++ src/LinkedList/Questions/Intersection_BruteForce.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package LinkedList.Questions;
 
 class ListNode {
     int val;;
--- src/LinkedList/Questions/LinkedListTraversal.java
+++ src/LinkedList/Questions/LinkedListTraversal.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package LinkedList.Questions;
 
 public class LinkedListTraversal {
 
