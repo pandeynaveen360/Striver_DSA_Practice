8f9747d06ca75b20ad03eed43acd30f55ad82e3b
naveenpandey <naveenppandey8080@gmail.com>
2025-11-07 22:08:02 +0530

    Starting_Node_Of_Cycle

---
+++ src/LinkedList/Questions/Starting_Node_Of_Cycle.java
@@ -0,0 +1,51 @@
+package LinkedList.Questions;
+
+public class Starting_Node_Of_Cycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                ListNode start = head;
+                while (start != slow) {
+                    start = start.next;
+                    slow = slow.next;
+                }
+                return start;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+
+        head.next.next.next.next = head.next;
+
+        Starting_Node_Of_Cycle obj = new Starting_Node_Of_Cycle();
+        ListNode cycleStart = obj.detectCycle(head);
+
+        System.out.println(cycleStart.val);
+
+    }
+}
