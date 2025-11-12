ce4c36da42e056e690297be7cdf5f70a4ce3c866
naveenpandey <naveenppandey8080@gmail.com>
2025-11-12 14:51:00 +0530

    stack and queue

---
+++ src/Queue/InBuildExamples.java
@@ -0,0 +1,20 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InBuildExamples {
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        queue.add(60);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+    }
+}
