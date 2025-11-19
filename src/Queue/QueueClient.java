3b9cb0acc0c5ca4dfa2dd7c59e6c5f14bd5b7d2f
naveenpandey <naveenppandey8080@gmail.com>
2025-11-19 10:44:01 +0530

    queue

---
+++ src/Queue/QueueClient.java
@@ -0,0 +1,19 @@
+package Queue;
+
+public class QueueClient {
+    public static void main(String[] args) {
+
+        QueueUsingLL queue = new QueueUsingLL();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        System.out.println(queue.dequeue());
+        queue.display();
+        queue.enqueue(40);
+        queue.display();
+        System.out.println(queue.dequeue());
+        queue.display();
+
+    }
+}
